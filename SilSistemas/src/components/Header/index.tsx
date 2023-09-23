ff55839952b7f6c78f403e3e750eb3edddccda54
arctavares <cosilk@hotmail.com>
2023-09-23 15:05:03 -0300

    create header

---
+++ SilSistemas/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import '../Header/Header.css';
+import icon from '../../icons/favicon.png';
+
+function Header() {
+    return (
+      <div className="header">
+            <div className='headerContainer'>
+                <img src={icon} />
+                <h1>SIL</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
